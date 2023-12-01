f28bca33a8cd978783101b11b2d056612e945731
explorerII <q8956895@gmail.com>
2023-12-01 17:27:50 +0800

    create model: voteItem, voteRecord

---
+++ src/main/java/com/alvis/votingsystem/model/VoteItem.java
@@ -0,0 +1,52 @@
+package com.alvis.votingsystem.model;
+
+import java.util.Date;
+
+public class VoteItem {
+
+    private Integer itemId;
+    private String itemName;
+    private Integer itemStatus;
+    private Date createDate;
+    private Date lastModifiedDate;
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getItemStatus() {
+        return itemStatus;
+    }
+
+    public void setItemStatus(Integer itemStatus) {
+        this.itemStatus = itemStatus;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
---
+++ src/main/java/com/alvis/votingsystem/model/VoteRecord.java
@@ -0,0 +1,43 @@
+package com.alvis.votingsystem.model;
+
+import java.util.Date;
+
+public class VoteRecord {
+
+    private Integer recordId;
+    private String recordName;
+    private Integer itemId;
+    private Date createDate;
+
+    public Integer getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(Integer recordId) {
+        this.recordId = recordId;
+    }
+
+    public String getRecordName() {
+        return recordName;
+    }
+
+    public void setRecordName(String recordName) {
+        this.recordName = recordName;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
